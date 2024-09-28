e27b7139130b5e8b5e9aaa129926466bba22e9d6
Yuichi Nakamura <yuichi.nakamura@jmty.jp>
2024-09-28 23:45:30 +0900

    TransactionにWalletの秘密鍵で署名できるようにする

--- lib/bc_ruby/transaction.rb
+++ lib/bc_ruby/transaction.rb
@@ -1,12 +1,14 @@
 require 'json'
+require 'openssl'
 
 class Transaction
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :signature
 
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
+    @signature = nil
   end
 
   def to_hash
@@ -19,5 +21,25 @@ class Transaction
 
   def to_json(*args)
     to_hash.to_json(*args)
+  end
+
+  # トランザクションに署名を追加
+  def sign_transaction(private_key)
+    raise 'マイニング報酬には署名が不要です' if @sender.nil?
+
+    digest = OpenSSL::Digest::SHA256.new
+    data = to_json
+    @signature = private_key.sign(digest, data)
+  end
+
+  # 署名の検証
+  def is_valid?(public_key)
+    return true if @sender.nil? # マイニング報酬は署名不要
+
+    raise '署名がありません' if @signature.nil?
+
+    digest = OpenSSL::Digest::SHA256.new
+    data = to_json
+    public_key.verify(digest, @signature, data)
   end
 end
--- spec/bc_ruby/transaction_spec.rb
+++ spec/bc_ruby/transaction_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Transaction do
       expect(tx.sender).to eq('Alice')
       expect(tx.receiver).to eq('Bob')
       expect(tx.amount).to eq(50)
+      expect(tx.signature).to be_nil
     end
   end
 
@@ -21,6 +22,42 @@ RSpec.describe Transaction do
       tx = Transaction.new('Alice', 'Bob', 50)
       expected_json = '{"sender":"Alice","receiver":"Bob","amount":50}'
       expect(tx.to_json).to eq(expected_json)
+    end
+  end
+
+  describe '#sign_transaction' do
+    it 'トランザクションに署名を追加できること' do
+      wallet = Wallet.new
+      tx = Transaction.new(wallet.address, 'Bob', 50)
+      tx.sign_transaction(wallet.private_key)
+      expect(tx.signature).not_to be_nil
+    end
+
+    it 'マイニング報酬には署名を追加しないこと' do
+      tx = Transaction.new(nil, 'Bob', 100)
+      expect {tx.sign_transaction(nil) }.to raise_error('マイニング報酬には署名が不要です')
+    end
+  end
+
+  describe '#is_valid?' do
+    it '有効な署名を検証できること' do
+      wallet = Wallet.new
+      tx = Transaction.new(wallet.address, 'Bob', 50)
+      tx.sign_transaction(wallet.private_key)
+      expect(tx.is_valid?(wallet.public_key)).to be true
+    end
+
+    it '無効な署名を検証できること' do
+      wallet1 = Wallet.new
+      wallet2 = Wallet.new
+      tx = Transaction.new(wallet1.address, 'Bob', 50)
+      tx.sign_transaction(wallet2.private_key) # 間違った秘密鍵で署名
+      expect(tx.is_valid?(wallet1.public_key)).to be false
+    end
+
+    it 'マイニング報酬は常に有効であること' do
+      tx = Transaction.new(nil, 'Bob', 100)
+      expect(tx.is_valid?(nil)).to be true
     end
   end
 end
